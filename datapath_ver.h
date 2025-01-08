6a800912ade22215fb89cf03fdd6f0fb68e3fdea
Release Engineer <ReleaseEngineer@maxlinear.com>
2025-01-08 10:16:22 +0200

    URX PRPLOS OSS Release UPDK_9.1.80

---
+++ datapath_ver.h
@@ -0,0 +1,17 @@
+/*****************************************************************************
+ * Copyright (c) 2021 - 2024, MaxLinear, Inc.
+ *
+ * For licensing information, see the file 'LICENSE' in the root folder of
+ * this software module.
+
+*******************************************************************************/
+
+#ifndef DATAPATH_VER_H_
+#define DATAPATH_VER_H_
+
+#define DP_VER_MAJ 1
+#define DP_VER_MID 0
+#define DP_VER_MIN 0
+#define DP_VER_TAG 206
+
+#endif
